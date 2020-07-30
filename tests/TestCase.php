4bce530127d105a8b5d97628df2403f11c52f768
Laravel Shift <shift@laravelshift.com>
2020-07-30 03:01:19 +0000

    Include helpful testing packages
    Shift added the following packages which may be used by some of the generated tests cases and helpful for testing new code.
    
    - [Test Assertions](https://github.com/jasonmccreary/laravel-test-assertions)
    - [`generate:model-factory`](https://github.com/mpociot/laravel-test-factory-helper)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,11 @@
 
 namespace Tests;
 
+use JMac\Testing\Traits\AdditionalAssertions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, RefreshDatabase;
+    use CreatesApplication, AdditionalAssertions, RefreshDatabase;
 }
