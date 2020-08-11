6ef1f3ed631251ccc281c75918f278c044a2a9d0
Sniper7Kills <willg@1337hax.org>
2020-08-10 20:00:12 -0400

    Command to create initial providers

---
+++ app/Console/Commands/AddRelayProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\RelayProvider;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class AddRelayProvider extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'relay:provider:create 
+                                {name : name of the provider}
+                                {type : Twitter or Discord}
+                                {user_id : ID of the user who "owns" the relay}
+                                {details : json encoded details of the provider}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new relay provider';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $provider = new RelayProvider();
+        $provider->name = $this->argument('name');
+        $provider->type = $this->argument('type');
+        $provider->details = $this->argument('details');
+        $provider->enabled = true;
+        $provider->user()->associate(User::find($this->argument('user_id')));
+        
+        $provider->save();
+
+        return 0;
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\AddRelayProvider;
 use App\Console\Commands\PreviousSiteDataSeeder;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         PreviousSiteDataSeeder::class,
+        AddRelayProvider::class
     ];
 
     /**
