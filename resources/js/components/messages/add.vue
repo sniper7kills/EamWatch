fecc9135279f7d3102190462a2fa2e4e6c5491ac
Will G <willg@1337hax.org>
2021-01-09 22:13:29 -0500

    try to use utc not localtime

--- resources/js/components/messages/add.vue
+++ resources/js/components/messages/add.vue
@@ -27,7 +27,7 @@
                         <div class="col-6">
                             <div class="form-group">
                                 <label for="time">Broadcasted at</label>
-                                <input v-model="time" type="datetime-local" id="time" class="form-control" v-bind:class="{ 'is-invalid': this.$errors.has('time') }" :disabled="submitting === true">
+                                <input v-model="time" type="datetime" id="time" class="form-control" v-bind:class="{ 'is-invalid': this.$errors.has('time') }" :disabled="submitting === true">
                                 <error input="time" class="text-danger" />
                             </div>
                         </div>
