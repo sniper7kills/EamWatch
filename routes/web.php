27c5082ffcfe28e6ab09d6705d3271a8135adba4
Shift <shift@laravelshift.com>
2022-02-25 17:06:39 +0000

    Shift cleanup

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SpaController;
-use Illuminate\Support\Facades\Route;
-
+use App\Http\Middleware\BannedMiddleware;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Http\Middleware\BannedMiddleware;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/banned', function () {
     return view('banned');
